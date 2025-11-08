b942bbe024d34484b600e992be9c73b8305bab96
Demorgon <DEMORGON@gmail.com>
2025-11-08 04:05:01 -0500

    Delete linux-video-encoder directory

--- linux-video-encoder/scripts/run_encoder.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script sets up the environment and runs the video encoder.
-
-# Activate the virtual environment if needed
-# source /path/to/your/venv/bin/activate
-
-# Run the encoder script
-python3 ../src/autoencoder.py "$@"
